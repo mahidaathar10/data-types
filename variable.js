4d03fca8952640e1dccd84e0b8f31aa7ff397c4a
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-03-10 22:29:25 +0500

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = ("mahida");
+console.log(userName);
+const personName = ("athar");
+console.log(personName);
+var newName = ("faiza");
+console.log(newName);
---
+++ variable.ts
@@ -0,0 +1,8 @@
+let userName :string = ("mahida");
+console .log(userName);
+
+const personName:string =("athar");
+console.log(personName);
+
+var newName: string = ("faiza");
+console.log(newName);
