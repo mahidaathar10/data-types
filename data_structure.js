4338935ad34a8994a4b7de49928e268ca89ef5e8
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-03-10 22:28:18 +0500

    Add files via upload

---
+++ data_structure.js
@@ -0,0 +1,7 @@
+"use strict";
+//array
+let personName = ["mahida", "mahnoor", "fizza", "sonia"];
+console.log(personName);
+console.log(personName[1], personName[2]);
+let newNumber = ["mahida", 1, 2, 3, 4, true];
+console.log(newNumber);
---
+++ data_structure.ts
@@ -0,0 +1,7 @@
+//array
+let personName: string[]=["mahida","mahnoor","fizza","sonia"];
+console.log(personName);
+console.log(personName[1],personName[2]);
+
+let newNumber: any[]=["mahida",1,2,3,4,true];
+console.log(newNumber);
