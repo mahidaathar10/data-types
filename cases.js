4ff8538c09d23d1c235db09d29ad9af1f4400f97
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-03-10 22:28:57 +0500

    Add files via upload

---
+++ cases.js
@@ -0,0 +1,8 @@
+"use strict";
+//example of camelCase
+let personName = "mahida";
+//example of snake_case
+let person_name = "sonia";
+//example of pascalcase
+let PersonName = "fizza";
+console.log(personName, person_name, PersonName);
---
+++ cases.ts
@@ -0,0 +1,7 @@
+//example of camelCase
+let personName = "mahida";
+//example of snake_case
+let person_name = "sonia";
+//example of pascalcase
+let PersonName = "fizza";
+console.log(personName,person_name,PersonName);
